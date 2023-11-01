a9381f5d2153c24674cd0e119c780531ea38e707
Kaufmann, Oliver (SI E SCM LPZ MF) <kaufmann.oliver@siemens.com>
2023-11-01 19:40:24 +0100

    feat: wip auth

---
+++ server/api/auth/signin.post.ts
@@ -0,0 +1,31 @@
+import { eq, or } from 'drizzle-orm';
+import { z } from 'zod';
+import { comparePassword } from '~/server/utils/auth/password';
+import { createSession } from '~/server/utils/auth/session';
+
+export default defineEventHandler(async (event) => {
+	const input = z
+		.object({
+			username: z.string(),
+			password: z.string()
+		})
+		.parse(await readBody(event));
+
+	const user = await db
+		.select()
+		.from(users)
+		.where(or(eq(users.email, input.username), eq(users.username, input.username)))
+		.limit(1)
+		.then((rows) => {
+			return rows[0];
+		});
+
+	if (!user) throw createError({ statusCode: 404, statusMessage: 'User not found!' });
+	if (!comparePassword(input.password, user.password, user.seed)) throw createError({ statusCode: 401, statusMessage: 'Invalid Password!' });
+
+	await createSession(event, user);
+
+	return {
+		success: true
+	};
+});
---
+++ server/api/auth/signup.post.ts
@@ -0,0 +1,65 @@
+import { eq, or, sql } from 'drizzle-orm';
+import { z } from 'zod';
+import { encryptPassword } from '~/server/utils/auth/password';
+import { createSession } from '~/server/utils/auth/session';
+import type { Permission } from '~/types/enum';
+
+export default defineEventHandler(async (event) => {
+	const input = z
+		.object({
+			username: z.string().min(4),
+			password: z.string().min(8),
+			email: z.string().email()
+		})
+		.parse(await readBody(event));
+
+	const userTest = await db
+		.select()
+		.from(users)
+		.where(or(eq(users.email, input.email), eq(users.username, input.username)))
+		.limit(1)
+		.then((rows) => {
+			return rows[0];
+		});
+
+	if (userTest) throw createError({ statusCode: 500, statusMessage: 'User already exsists!' });
+	const { password, seed } = encryptPassword(input.password);
+
+	const result = await db.select({ count: sql<number>`count(*)` }).from(users);
+	const permissions = result[0]?.count && result[0]?.count > 0 ? [] : ['super'];
+
+	await db.insert(users).values({
+		username: input.username,
+		password,
+		seed,
+		email: input.email
+	});
+
+	const user = await db
+		.select()
+		.from(users)
+		.where(eq(users.email, input.email))
+		.limit(1)
+		.then((rows) => {
+			return rows[0];
+		});
+
+	if (!user) throw createError({ statusCode: 500, statusMessage: 'Something went wrong!' });
+
+	if (permissions.length > 0) {
+		await Promise.all(
+			permissions.map((perm) => {
+				return db.insert(permission).values({
+					userId: user.id,
+					permission: perm as Permission
+				});
+			})
+		);
+	}
+
+	await createSession(event, user);
+
+	return {
+		success: true
+	};
+});
